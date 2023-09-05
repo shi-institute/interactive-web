907004f4fc4d1da5c2241ee89b0782fec6bd0316
Jack Buehner <jackb@furman.edu>
2023-09-05 11:41:43 -0400

    feat: add PageTitle component

---
+++ src/lib/PageTitle.svelte
@@ -0,0 +1,22 @@
+<div class="page-title-wrapper">
+  <h1><slot /></h1>
+  <p class="caption"><slot name="caption" /></p>
+</div>
+
+<style>
+  .page-title-wrapper {
+    background-color: #212121;
+    color: #e0e0e0;
+    padding: 10px;
+    margin: 0;
+  }
+
+  h1 {
+    margin: 0;
+    font-size: 1.5em;
+  }
+
+  .caption {
+    margin: 0 0 4px 0;
+  }
+</style>
