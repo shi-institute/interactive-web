a91f9208ab00aa72218c499493a9280e8d390048
Jack Buehner <jackb@furman.edu>
2023-09-05 13:25:07 -0400

    feat: add legend components

---
+++ src/lib/legend/Legend.svelte
@@ -0,0 +1,22 @@
+<aside class="legend">
+  <h1>Legend</h1>
+  <slot />
+</aside>
+
+<style>
+  h1 {
+    font-size: 16px;
+    margin: 0 0 6px 0;
+  }
+
+  .legend {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background-color: white;
+    z-index: 400;
+    padding: 10px;
+    border-radius: 3px;
+    box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);
+  }
+</style>
---
+++ src/lib/legend/LegendEntry.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  export let type: 'rectangle' | 'rect' | 'square' = 'rectangle';
+  export let outline: string;
+  export let fill: string = 'transparent';
+</script>
+
+<div class="entry" style="--outline: {outline}; --fill: {fill};">
+  {#if type === 'rectangle' || type === 'rect'}
+    <div class="rect" />
+  {/if}
+  {#if type === 'square'}
+    <div class="square" />
+  {/if}
+  <p><slot /></p>
+</div>
+
+<style>
+  .entry {
+    display: flex;
+    flex-direction: row;
+    gap: 5px;
+    align-items: center;
+  }
+
+  p {
+    margin: 0;
+    font-size: 14px;
+  }
+
+  div.rect {
+    display: inline-block;
+    border: 2px solid var(--outline);
+    background: var(--fill);
+    width: 16px;
+    height: 10px;
+    margin-top: 1px;
+  }
+
+  div.square {
+    display: inline-block;
+    border: 2px solid var(--outline);
+    background: var(--fill);
+    width: 10px;
+    height: 10px;
+    margin-top: 1px;
+    margin-right: 6px;
+  }
+</style>
