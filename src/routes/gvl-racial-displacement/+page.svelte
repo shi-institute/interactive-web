07c218d893dfecc92f6d31345363078c0f828a64
Jack Buehner <jackb@furman.edu>
2023-09-12 16:42:57 -0400

    feat: add racial displacement storymap

---
+++ src/routes/gvl-racial-displacement/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Frame from '$lib/Frame.svelte';
+</script>
+
+<svelte:head>
+  <title>Racial displacement in Greenville, SC</title>
+</svelte:head>
+
+<Frame src="https://storymaps.arcgis.com/stories/ec193fcd3f194bc0a7f8353f69f24aa8" />
