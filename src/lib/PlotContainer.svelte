b21c5d719d8e056ba0110e805ae7ca515d262116
Jack Buehner <jackb@furman.edu>
2024-05-10 15:18:35 -0400

    feat: add svelte component for creating plots

---
+++ src/lib/PlotContainer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import * as Plot from '@observablehq/plot';
+
+  export let plot: Plot.PlotOptions;
+  export let fullWidth = false;
+
+  let clientWidth: number | undefined;
+  let div: HTMLDivElement;
+  $: {
+    div?.firstChild?.remove(); // remove old chart if it exists
+    div?.append(Plot.plot({ ...plot, width: fullWidth ? clientWidth : plot.width || 640 }));
+  }
+</script>
+
+<div bind:clientWidth="{clientWidth}">
+  <div bind:this="{div}" role="img"></div>
+</div>
