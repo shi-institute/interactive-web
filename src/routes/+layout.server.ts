16300d7a581a835781762d650e5d3b10076790e6
Jack Buehner <jackb@furman.edu>
2024-05-29 14:12:34 -0400

    fix: use `Object.groupBy` polyfill everywhere

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,8 @@
 import { PROTECTED_PAGE_PASSWORD } from '$env/static/private';
 import type { LayoutServerLoad } from './$types';
 
+import 'groupby-polyfill/lib/polyfill.js';
+
 export const load = (async ({ locals }) => {
   // handle authentication
   const { counter = 0, protectedPass = '' } = locals.session.data;
