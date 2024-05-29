ea2f879abe33ba900f5a7290df3c367f7878bcfd
Jack Buehner <jackb@furman.edu>
2024-05-29 16:52:50 -0400

    feat: use html select and option instead of custom dropdown for mobile friendliness

--- src/routes/research/household-income-bracket-race/SubtitleSelect.svelte
+++ src/routes/research/household-income-bracket-race/SubtitleSelect.svelte
@@ -1,27 +1,49 @@
 <script lang="ts">
-  import { MenuFlyout, MenuFlyoutItem } from 'fluent-svelte';
-
   export let options: string[] = [];
   export let value: string = options[0];
   export let disabled = false;
+
+  let width = 1200;
+
+  function updateSelectWidth(select: HTMLSelectElement) {
+    if (select?.options && select.selectedIndex > -1) {
+      const option = select.options[select.selectedIndex];
+      const span = document.createElement('span');
+
+      span.textContent = option.textContent;
+
+      const ostyles = getComputedStyle(option);
+      span.style.fontFamily = ostyles.fontFamily;
+      span.style.fontStyle = ostyles.fontStyle;
+      span.style.fontWeight = ostyles.fontWeight;
+      span.style.fontSize = ostyles.fontSize;
+
+      document.body.appendChild(span);
+      select.style.width = span.offsetWidth + 20 + 'px';
+      document.body.removeChild(span);
+    }
+  }
+
+  let selectElem: HTMLSelectElement;
+  $: if (selectElem) updateSelectWidth(selectElem);
 </script>
 
-<MenuFlyout alignment="start" placement="bottom">
-  <button class="subtitle-dropdown-button" disabled="{disabled}">
-    {value}
-    {disabled ? '' : '▾'}
-  </button>
-  <svelte:fragment slot="flyout">
-    {#each options as option}
-      <MenuFlyoutItem selected="{value === option}" on:click="{() => (value = option)}">
-        <span>{option}</span>
-      </MenuFlyoutItem>
-    {/each}
-  </svelte:fragment>
-</MenuFlyout>
+<svelte:window bind:innerWidth="{width}" />
+
+<select
+  bind:this="{selectElem}"
+  bind:value="{value}"
+  on:change="{(evt) => updateSelectWidth(evt.target)}"
+  disabled="{disabled}"
+>
+  {#each options as option}
+    <option value="{option}">{option}</option>
+  {/each}
+</select>
+<span style="margin-left: -20px;">{disabled ? '' : '▾'}</span>
 
 <style>
-  button {
+  select {
     appearance: none;
     border: none;
     background: none;
@@ -37,7 +59,11 @@
     margin-bottom: 0.25rem;
   }
 
-  button:not([disabled]):hover {
+  select:not([disabled]):hover {
     text-decoration: underline;
+  }
+
+  option {
+    background-color: var(--fds-solid-background-tertiary);
   }
 </style>
