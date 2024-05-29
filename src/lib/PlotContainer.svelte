50a1c796d66c8233b30531c4ce13ff41a4119e73
Jack Buehner <jackb@furman.edu>
2024-05-29 14:54:38 -0400

    fix: do not attempt to load a plot unless we are in the browser

--- src/lib/PlotContainer.svelte
+++ src/lib/PlotContainer.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { browser } from '$app/environment';
   import * as Plot from '@observablehq/plot';
 
   export let plot: Plot.PlotOptions;
@@ -7,8 +8,10 @@
   let clientWidth: number | undefined;
   let div: HTMLDivElement;
   $: {
-    div?.firstChild?.remove(); // remove old chart if it exists
-    div?.append(Plot.plot({ ...plot, width: fullWidth ? clientWidth : plot.width || 640 }));
+    if (browser) {
+      div?.firstChild?.remove(); // remove old chart if it exists
+      div?.append(Plot.plot({ ...plot, width: fullWidth ? clientWidth : plot.width || 640 }));
+    }
   }
 </script>
 
