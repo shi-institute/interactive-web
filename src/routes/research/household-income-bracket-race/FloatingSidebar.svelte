cf311e9e3208d1703f15c323d4d9b05d57677236
Jack Buehner <jackb@furman.edu>
2024-05-29 15:23:37 -0400

    feat: enable clicking options sidebar scrim to close the sidebar

--- src/routes/research/household-income-bracket-race/FloatingSidebar.svelte
+++ src/routes/research/household-income-bracket-race/FloatingSidebar.svelte
@@ -1,24 +1,44 @@
 <script>
   import { Button, IconButton, Tooltip } from 'fluent-svelte';
   import Sidebar from './Sidebar.svelte';
+  import { isPanelWrapperOpen } from './isPanelWrapperOpen';
 
   export let open = false;
+  $: {
+    // if the panel is open, wait 360 milliseconds before marking is closed
+    // to give the wrapper transition enough time to hide the panel and wrapper
+    if (open) {
+      setTimeout(() => {
+        open = $isPanelWrapperOpen;
+      }, 360);
+    } else {
+      open = $isPanelWrapperOpen;
+    }
+  }
 </script>
 
-<div class="floating-sidebar-wrapper" class:open="{open}">
-  <IconButton class="close-icon" on:click="{() => (open = false)}">
-    <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="m4.21 4.387.083-.094a1 1 0 0 1 1.32-.083l.094.083L12 10.585l6.293-6.292a1 1 0 1 1 1.414 1.414L13.415 12l6.292 6.293a1 1 0 0 1 .083 1.32l-.083.094a1 1 0 0 1-1.32.083l-.094-.083L12 13.415l-6.293 6.292a1 1 0 0 1-1.414-1.414L10.585 12 4.293 5.707a1 1 0 0 1-.083-1.32l.083-.094-.083.094Z"
-      ></path>
-    </svg>
-  </IconButton>
-  <Sidebar pageTitleElemVisibleHeight="{2}" noBorderLeft />
-</div>
+{#if open}
+  <div class="floating-sidebar-wrapper" class:open="{$isPanelWrapperOpen}">
+    <IconButton class="close-icon" on:click="{() => ($isPanelWrapperOpen = false)}">
+      <svg
+        width="24"
+        height="24"
+        fill="none"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="m4.21 4.387.083-.094a1 1 0 0 1 1.32-.083l.094.083L12 10.585l6.293-6.292a1 1 0 1 1 1.414 1.414L13.415 12l6.292 6.293a1 1 0 0 1 .083 1.32l-.083.094a1 1 0 0 1-1.32.083l-.094-.083L12 13.415l-6.293 6.292a1 1 0 0 1-1.414-1.414L10.585 12 4.293 5.707a1 1 0 0 1-.083-1.32l.083-.094-.083.094Z"
+        ></path>
+      </svg>
+    </IconButton>
+    <Sidebar pageTitleElemVisibleHeight="{2}" noBorderLeft />
+  </div>
+{/if}
 
-<div class="open-icon-wrapper" class:open="{open}">
+<div class="open-icon-wrapper" class:open="{$isPanelWrapperOpen}">
   <Tooltip text="Open page options" alignment="end">
-    <Button on:click="{() => (open = true)}" class="expand-button">
+    <Button on:click="{() => ($isPanelWrapperOpen = true)}" class="expand-button">
       <svg
         width="16"
         height="16"
@@ -36,7 +56,49 @@
   </Tooltip>
 </div>
 
+{#if open}
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div
+    class="scrim"
+    class:isOpen="{$isPanelWrapperOpen}"
+    on:click="{() => ($isPanelWrapperOpen = false)}"
+  ></div>
+{/if}
+
 <style>
+  .scrim {
+    position: fixed;
+    inset: 0;
+    z-index: 99;
+    background-color: #000000;
+    opacity: 0;
+    transition: opacity 360ms;
+    animation: close-scrim 360ms forwards;
+  }
+  .scrim.isOpen {
+    animation: open-scrim 360ms forwards;
+  }
+  @keyframes open-scrim {
+    0% {
+      display: block;
+      opacity: 0;
+    }
+    100% {
+      opacity: 0.12;
+    }
+  }
+  @keyframes close-scrim {
+    0% {
+      display: block;
+      opacity: 0.06;
+    }
+    100% {
+      opacity: 0;
+      display: none;
+    }
+  }
+
   .floating-sidebar-wrapper {
     position: fixed;
     right: 20px;
@@ -50,7 +112,7 @@
     transform: translateX(340px);
     box-shadow: var(--fds-dialog-shadow);
     opacity: 0;
-    z-index: 10;
+    z-index: 100;
   }
   .floating-sidebar-wrapper.open {
     transform: translateX(0);
