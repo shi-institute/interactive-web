586bfb2d150d7ab14c074b732a01027b66f17b29
Jack Buehner <jackb@furman.edu>
2024-06-07 12:05:38 -0400

    feat: add options bar to top of page

--- src/routes/research/household-income-bracket-race/+page.svelte
+++ src/routes/research/household-income-bracket-race/+page.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
   import { browser } from '$app/environment';
-  import { page } from '$app/stores';
   import EmbedBar from '$lib/EmbedBar.svelte';
   import PlotContainer from '$lib/PlotContainer.svelte';
   import { opWonkOptionsStore } from '$stores/opWonkOptionsStore';
   import { downloadElement } from '$utils/downloadElement';
   import { Button } from 'fluent-svelte';
-  import html2canvas from 'html2canvas';
   import type { PageData } from './$types';
   import FloatingSidebar from './FloatingSidebar.svelte';
   import Legend from './Legend.svelte';
+  import OptionsBar from './OptionsBar.svelte';
   import SubtitleSelect from './SubtitleSelect.svelte';
   import { isPanelWrapperOpen } from './isPanelWrapperOpen';
 
@@ -59,69 +58,13 @@
 
 <FloatingSidebar handleExport="{downloadFigures}" isEmbedded="{isEmbedded}" />
 
+{#if !isEmbedded}
+  <OptionsBar />
+{/if}
+
 <div class="exportable" bind:this="{exportElem}">
   <div class="header">
     <h1>Households by income bracket and race</h1>
-    {#if !isEmbedded}
-      <div class="buttons" data-html2canvas-ignore>
-        {#if $opWonkOptionsStore.compare}
-          <Button on:click="{() => ($opWonkOptionsStore.compare = false)}">
-            <svg
-              width="20"
-              height="20"
-              fill="none"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M2 6.25A2.25 2.25 0 0 1 4.25 4h15.5A2.25 2.25 0 0 1 22 6.25v11.5A2.25 2.25 0 0 1 19.75 20H4.25A2.25 2.25 0 0 1 2 17.75V6.25Zm2.25-.75a.75.75 0 0 0-.75.75v11.5c0 .414.336.75.75.75h15.5a.75.75 0 0 0 .75-.75V6.25a.75.75 0 0 0-.75-.75H4.25Z"
-                fill="#ffffff"
-              ></path>
-              <path
-                d="M8.22 8.215a.75.75 0 0 1 1.06 0l2.72 2.72 2.725-2.716a.75.75 0 0 1 1.06 1.062l-2.724 2.715 2.724 2.724a.75.75 0 1 1-1.06 1.06L12 13.057 9.28 15.78a.75.75 0 0 1-1.06-1.06l2.72-2.724-2.72-2.72a.75.75 0 0 1 0-1.06Z"
-                fill="#ffffff"
-              ></path>
-            </svg>
-            Stop comparing
-          </Button>
-        {:else}
-          <Button on:click="{() => ($opWonkOptionsStore.compare = true)}">
-            <svg
-              width="20"
-              height="20"
-              fill="none"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M8.31 10.248a.75.75 0 1 0-1.12-.997l-2 2.25a.75.75 0 0 0 0 .997l2 2.25a.75.75 0 1 0 1.12-.996l-.89-1.002h9.16l-.89 1.002a.75.75 0 1 0 1.12.996l2-2.25.01-.012a.747.747 0 0 0-.012-.987L16.81 9.251a.75.75 0 1 0-1.12.997l.89 1.002H7.42l.89-1.002Z"
-                fill="currentColor"
-              ></path>
-              <path
-                d="M12.754 4.001v.002h7.498c.966 0 1.75.784 1.75 1.75v12.495a1.75 1.75 0 0 1-1.75 1.75h-8.998v-.002H3.757a1.75 1.75 0 0 1-1.75-1.75V5.751c0-.967.783-1.75 1.75-1.75h8.997Zm7.498 1.502h-7.498V10.5h-1.5V5.5H3.757a.25.25 0 0 0-.25.25v12.495c0 .138.112.25.25.25h7.497V13.5h1.5v4.998h7.498a.25.25 0 0 0 .25-.25V5.754a.25.25 0 0 0-.25-.25Z"
-                fill="currentColor"
-              ></path>
-            </svg>
-            Compare
-          </Button>
-        {/if}
-        <Button on:click="{() => ($isPanelWrapperOpen = !$isPanelWrapperOpen)}">
-          <svg
-            width="16"
-            height="16"
-            fill="none"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M8.75 13.5a3.251 3.251 0 0 1 3.163 2.498L21.25 16a.75.75 0 0 1 .102 1.493l-.102.007h-9.337a3.251 3.251 0 0 1-6.326 0H2.75a.75.75 0 0 1-.102-1.493L2.75 16h2.837a3.251 3.251 0 0 1 3.163-2.5Zm0 1.5a1.75 1.75 0 0 0-1.652 1.172l-.021.063-.039.148a1.756 1.756 0 0 0 .02.815l.04.13.025.069a1.75 1.75 0 0 0 3.28-.069l.04-.13-.018.06a1.75 1.75 0 0 0 .048-.815l-.03-.137-.02-.07-.047-.134A1.75 1.75 0 0 0 8.75 15Zm6.5-11a3.251 3.251 0 0 1 3.163 2.5h2.837a.75.75 0 0 1 .102 1.493L21.25 8h-2.837a3.251 3.251 0 0 1-6.326 0H2.75a.75.75 0 0 1-.102-1.493L2.75 6.5l9.337-.002A3.251 3.251 0 0 1 15.25 4Zm0 1.5a1.75 1.75 0 0 0-1.652 1.173l-.021.062-.038.148a1.757 1.757 0 0 0 .019.815l.04.13.025.069a1.75 1.75 0 0 0 3.28-.068l.04-.131-.018.06a1.75 1.75 0 0 0 .048-.815l-.03-.137-.02-.07-.047-.134A1.75 1.75 0 0 0 15.25 5.5Z"
-              fill="currentColor"
-            ></path>
-          </svg>
-          Options
-        </Button>
-      </div>
-    {/if}
   </div>
 
   <div class="figures" class:exporting="{exporting}" style="--exportWidth: {exportWidth}px;">
@@ -261,27 +204,6 @@
 
   .subtitle {
     user-select: none;
-  }
-
-  .buttons {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    width: 150px;
-    justify-content: right;
-    gap: 6px;
-    position: absolute;
-    top: 20px;
-    right: 20px;
-  }
-  @media (max-width: 700px) {
-    .buttons {
-      display: none;
-    }
-  }
-
-  .buttons svg {
-    margin-right: 6px;
   }
 
   .figures {
---
+++ src/routes/research/household-income-bracket-race/OptionsBar.svelte
@@ -0,0 +1,160 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  import { opWonkOptionsStore } from '$stores/opWonkOptionsStore';
+  import { Button, TextBlock, ToggleSwitch } from 'fluent-svelte';
+  import { isPanelWrapperOpen } from './isPanelWrapperOpen';
+
+  let windowWidth = 1000;
+</script>
+
+<svelte:window bind:innerWidth="{windowWidth}" />
+
+<div class="button-bar">
+  <div>
+    <TextBlock variant="caption">
+      Municipality 1
+      {#if windowWidth > 1200}
+        (left)
+      {:else}
+        (top)
+      {/if}
+    </TextBlock>
+    <select bind:value="{$opWonkOptionsStore.city}">
+      {#each $page.data.citiesUnique as city}
+        <option value="{city}">{city}</option>
+      {/each}
+    </select>
+  </div>
+
+  {#if $opWonkOptionsStore.compare}
+    <div>
+      <TextBlock variant="caption" class="cdrz-info-sidebar--field-title">
+        Municipality 2
+        {#if windowWidth > 1200}
+          (right)
+        {:else}
+          (bottom)
+        {/if}
+      </TextBlock>
+      <select bind:value="{$opWonkOptionsStore.city2}">
+        {#each $page.data.citiesUnique as city}
+          <option value="{city}">{city}</option>
+        {/each}
+      </select>
+    </div>
+  {/if}
+
+  <div>
+    <TextBlock variant="caption">Year range</TextBlock>
+    <select bind:value="{$opWonkOptionsStore.yearsACS}">
+      {#each $page.data.acsYearsUnique as yearRange}
+        <option value="{yearRange}">{yearRange}</option>
+      {/each}
+    </select>
+  </div>
+
+  <div>
+    <Button on:click="{() => ($isPanelWrapperOpen = !$isPanelWrapperOpen)}">
+      <svg
+        width="16"
+        height="16"
+        fill="none"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        style="margin-right: 8px;"
+      >
+        <path
+          d="M8.75 13.5a3.251 3.251 0 0 1 3.163 2.498L21.25 16a.75.75 0 0 1 .102 1.493l-.102.007h-9.337a3.251 3.251 0 0 1-6.326 0H2.75a.75.75 0 0 1-.102-1.493L2.75 16h2.837a3.251 3.251 0 0 1 3.163-2.5Zm0 1.5a1.75 1.75 0 0 0-1.652 1.172l-.021.063-.039.148a1.756 1.756 0 0 0 .02.815l.04.13.025.069a1.75 1.75 0 0 0 3.28-.069l.04-.13-.018.06a1.75 1.75 0 0 0 .048-.815l-.03-.137-.02-.07-.047-.134A1.75 1.75 0 0 0 8.75 15Zm6.5-11a3.251 3.251 0 0 1 3.163 2.5h2.837a.75.75 0 0 1 .102 1.493L21.25 8h-2.837a3.251 3.251 0 0 1-6.326 0H2.75a.75.75 0 0 1-.102-1.493L2.75 6.5l9.337-.002A3.251 3.251 0 0 1 15.25 4Zm0 1.5a1.75 1.75 0 0 0-1.652 1.173l-.021.062-.038.148a1.757 1.757 0 0 0 .019.815l.04.13.025.069a1.75 1.75 0 0 0 3.28-.068l.04-.131-.018.06a1.75 1.75 0 0 0 .048-.815l-.03-.137-.02-.07-.047-.134A1.75 1.75 0 0 0 15.25 5.5Z"
+          fill="currentColor"
+        ></path>
+      </svg>
+      More options
+    </Button>
+  </div>
+
+  <div>
+    <ToggleSwitch bind:checked="{$opWonkOptionsStore.compare}">
+      Compare two municipalities
+    </ToggleSwitch>
+  </div>
+</div>
+<div class="options-hint-bar">
+  <Button variant="hyperlink" on:click="{() => ($isPanelWrapperOpen = true)}">
+    Compare and change municipalities
+  </Button>
+</div>
+
+<style>
+  .button-bar,
+  .options-hint-bar {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    justify-content: flex-start;
+    padding: 10px;
+    background-color: #212121;
+    border-bottom: 1px solid #3d3d3d;
+    gap: 10px;
+  }
+  .button-bar > div {
+    display: flex;
+    flex-direction: column;
+    gap: 3px;
+  }
+  .button-bar > div:last-of-type {
+    flex-grow: 1;
+    align-items: flex-end;
+  }
+
+  @media (max-width: 950px) {
+    .button-bar {
+      display: none;
+    }
+  }
+  @media (min-width: 951px) {
+    .options-hint-bar {
+      display: none;
+    }
+  }
+
+  select {
+    align-items: center;
+    border: none;
+    border-radius: var(--fds-control-corner-radius);
+    box-sizing: border-box;
+    cursor: default;
+    display: inline-flex;
+    font-family: var(--fds-font-family-text);
+    font-size: var(--fds-body-font-size);
+    font-weight: 400;
+    justify-content: center;
+    line-height: 20px;
+    outline: none;
+    padding-block: 4px 6px;
+    padding-inline: 11px;
+    position: relative;
+    text-decoration: none;
+    transition: var(--fds-control-faster-duration) ease background;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    background-clip: padding-box;
+    background-color: var(--fds-control-fill-default);
+    border: 1px solid;
+    border-color: var(--fds-control-border-default);
+    color: var(--fds-text-primary);
+    width: 100%;
+  }
+  select:hover {
+    background-color: var(--fds-control-fill-secondary);
+  }
+  select:active {
+    background-color: var(--fds-control-fill-tertiary);
+    border-color: var(--fds-control-stroke-default);
+    color: var(--fds-text-secondary);
+  }
+  option {
+    background-color: var(--fds-solid-background-tertiary);
+  }
+</style>
