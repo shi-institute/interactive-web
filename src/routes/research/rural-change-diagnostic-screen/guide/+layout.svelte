15cbadd918e53cb750db2572d192635122a026eb
Jack Buehner <jackb@furman.edu>
2024-10-30 18:01:42 -0400

    feat: add home page and side navigation to guide

--- src/routes/research/rural-change-diagnostic-screen/guide/+layout.svelte
+++ src/routes/research/rural-change-diagnostic-screen/guide/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
   import { page } from '$app/stores';
+  import { hasKey } from '$utils';
+
+  export let data;
 
   $: title = $page.data.currentPage?.attributes?.title;
 </script>
@@ -12,15 +15,95 @@
   {/if}
 </svelte:head>
 
-<article>
-  {#if title}
-    <h1>{title}</h1>
-  {/if}
+<div class="grid">
+  <nav>
+    <ul>
+      <li>
+        <a href="/research/rural-change-diagnostic-screen/guide">
+          <span class="title">Home</span>
+          <span class="caption">How to use this tool</span>
+        </a>
+      </li>
+      {#each data.docsPageData as { pathname, attributes }}
+        {#if hasKey(attributes, 'title')}
+          <li>
+            <a href="{pathname}"><span class="title">{attributes.title}</span></a>
+          </li>
+        {/if}
+      {/each}
+    </ul>
+  </nav>
 
-  <slot />
-</article>
+  <div class="article">
+    <article>
+      {#if title}
+        <h1>{title}</h1>
+      {/if}
+
+      <slot />
+    </article>
+  </div>
+</div>
 
 <style>
+  .grid {
+    display: grid;
+    grid-template-columns: 240px 1fr;
+    height: 100%;
+  }
+
+  @media (max-width: 800px) {
+    .grid {
+      grid-template-columns: 1fr;
+    }
+  }
+
+  nav {
+    border-right: 1px solid var(--calcite-navigation-border-color, var(--calcite-color-border-3));
+  }
+
+  nav ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    user-select: none;
+  }
+
+  nav li {
+    border-bottom: 1px solid var(--calcite-navigation-border-color, var(--calcite-color-border-3));
+  }
+
+  nav li a {
+    display: block;
+    padding: 10px 20px;
+    color: var(--calcite-color-text-1);
+    text-decoration: none;
+  }
+
+  nav li:hover a span.title {
+    text-decoration: underline;
+  }
+
+  nav li:hover {
+    background-color: var(--calcite-color-foreground-2);
+  }
+
+  nav li:active {
+    background-color: var(--calcite-color-foreground-3);
+  }
+
+  nav li a span.caption {
+    font-size: 12px;
+    font-style: italic;
+    color: var(--calcite-color-text-3);
+    display: block;
+    text-decoration: none;
+  }
+
+  .article {
+    overflow: auto;
+  }
+
   article {
     margin: 0 auto;
     width: 100%;
--- src/routes/research/rural-change-diagnostic-screen/guide/+page.svelte
+++ src/routes/research/rural-change-diagnostic-screen/guide/+page.svelte
@@ -1,13 +1,41 @@
-<script lang="ts">
-  export let data;
-</script>
+<h2>How to use this tool</h2>
 
 <section>
-  <ul>
-    {#each data.docsPageData as { pathname, attributes }}
-      <li>
-        <a href="{pathname}">{attributes.title}</a>
-      </li>
-    {/each}
-  </ul>
+  <h3>Navigating and exploring</h3>
+
+  <article>
+    <h4>Showing and hiding layers</h4>
+  </article>
+
+  <article>
+    <h4>Changing the basemap</h4>
+  </article>
+
+  <article>
+    <h4>Viewing the map legend</h4>
+  </article>
+
+  <article>
+    <h4>Navigating the map</h4>
+  </article>
+
+  <article>
+    <h4>Find a specific tract or ZCTA</h4>
+  </article>
+
+  <article>
+    <h4>Using popups</h4>
+  </article>
+</section>
+
+<h3>Analysis</h3>
+
+<section>
+  <article>
+    <h4>Filtering the visible areas of interest</h4>
+  </article>
+
+  <article>
+    <h4>Compare multiple tracts and ZCTAs</h4>
+  </article>
 </section>
