1a3362885a77eac919484f489005a271cb5d8923
Jack Buehner <jackb@furman.edu>
2024-11-14 13:02:29 -0500

    feat: require password to access rural change diagnostic screening tool

--- src/routes/research/rural-change-diagnostic-screen/+layout.server.ts
+++ src/routes/research/rural-change-diagnostic-screen/+layout.server.ts
@@ -1,8 +1,18 @@
+import { _ensureAuthentication } from '../authenticate/+page.server';
 import type { LayoutServerLoad } from './$types';
 
 export const ssr = false;
 export const prerender = false;
 
-export const load = (async () => {
+export const load = (async ({ parent, url }) => {
+  const { session } = await parent();
+
+  _ensureAuthentication({
+    session,
+    url,
+    scope: 'application__rural_change_diagnostic_screen',
+    appName: 'Rural change diagnostic screening tool',
+  });
+
   return {};
 }) satisfies LayoutServerLoad;
