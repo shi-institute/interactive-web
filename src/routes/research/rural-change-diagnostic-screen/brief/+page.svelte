603af0e0db7861b28f7829d1ff80d68aad1e35c3
Jack Buehner <jackb@furman.edu>
2025-01-03 16:05:19 -0500

    feat(rcdst): add research brief

--- src/routes/research/rural-change-diagnostic-screen/brief/+page.svelte
+++ src/routes/research/rural-change-diagnostic-screen/brief/+page.svelte
@@ -1,1 +1,9 @@
-BRIEF
+<iframe
+  title="research brief"
+  src="/filestore/Kolb - Measuring and Mapping Rural Gentrification - Research Brief.pdf"
+  style="width: 100%; height: 100%;"
+  frameborder="0"
+  scrolling="no"
+>
+  <p>It appears your web browser doesn't support iframes.</p>
+</iframe>
