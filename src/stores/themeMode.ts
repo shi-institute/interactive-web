54e2b12f839dacf6b3167cba9c54430b6064f633
Jack Buehner <jackb@furman.edu>
2025-02-14 18:21:56 -0500

    feat: add theme mode toggle button to titlebar
    This button allows overrides prefers-color-scheme media queries. It allows forcing light mode and dark mode. Click to switch between forced light mode, forced, dark mode, and auto (default) mode.

--- src/stores/themeMode.ts
+++ src/stores/themeMode.ts
@@ -1,6 +1,16 @@
 import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
+import selectedThemeMode from './selectedThemeMode';
 
 const prefersDarkMode = browser && !!window?.matchMedia('(prefers-color-scheme: dark)').matches;
 
 export const themeMode = writable<'light' | 'dark'>(prefersDarkMode ? 'dark' : 'light');
+
+selectedThemeMode.subscribe((mode) => {
+  if (mode === 'auto') {
+    const prefersDarkMode = browser && !!window?.matchMedia('(prefers-color-scheme: dark)').matches;
+    themeMode.set(prefersDarkMode ? 'dark' : 'light');
+    return;
+  }
+  themeMode.set(mode);
+});
