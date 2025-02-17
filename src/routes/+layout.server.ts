e35bafcafc04de9dabc024be9e9949b20430e248
Jack Buehner <jackb@furman.edu>
2025-02-17 14:59:40 -0500

    feat: infer `isEmbedded` boolean (#22)

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,7 +2,7 @@ import 'groupby-polyfill/lib/polyfill.js';
 import type { LayoutServerLoad } from './$types';
 import { _calculateAuthScopes } from './research/authenticate/+page.server';
 
-export const load = (async ({ locals, url }) => {
+export const load = (async ({ locals }) => {
   const { counter = 0 } = locals.session.data;
 
   // an object of scopes and whether they are authenticated
@@ -14,5 +14,5 @@ export const load = (async ({ locals, url }) => {
     authScopes,
   });
 
-  return { session: locals.session.data, isEmbedded: url.searchParams.get('embedded') === '1' };
+  return { session: locals.session.data };
 }) satisfies LayoutServerLoad;
