63f713102873aab657514ccd62215a3a545fa523
Jack Buehner <jackb@furman.edu>
2025-05-12 14:53:53 -0400

    feat: add redirect to sterlinng neighborhood study at domain/sterling (#38)

--- src/redirects.ts
+++ src/redirects.ts
@@ -1,3 +1,5 @@
 export const redirects: Record<string, string> = {
   '/rcdst': '/research/rural-change-diagnostic-screen',
+  '/sterling':
+    'https://gis.furman.edu/portal/apps/storymaps/stories/9668f9a4f3734f748eaffb474e6073a8',
 };
