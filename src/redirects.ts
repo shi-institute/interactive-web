626129f3b80bf6c8e49acc924f4a721a180f0358
Jack Buehner <jackb@furman.edu>
2025-06-10 16:22:27 -0400

    feat: add sterling transportation storymap redirects

--- src/redirects.ts
+++ src/redirects.ts
@@ -4,4 +4,7 @@ export const redirects: Record<string, string> = {
     'https://gis.furman.edu/portal/apps/storymaps/stories/9668f9a4f3734f748eaffb474e6073a8',
   '/sterling/online': 'https://storymaps.arcgis.com/stories/9668f9a4f3734f748eaffb474e6073a8',
   '/sterling': '/sterling/online',
+  '/sterling/transportation/portal': 'https://gis.furman.edu/portal/apps/storymaps/stories/399e1df1f0104fb69ce9f7260e4b1a0c',
+  '/sterling/transportation/online': 'https://storymaps.arcgis.com/stories/399e1df1f0104fb69ce9f7260e4b1a0c',
+  '/sterling/transportation': '/sterling/transportation/online',
 };
