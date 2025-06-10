4f3c5852d87c04641661d0fcc0217f4500348f1d
Jack Buehner <jackb@furman.edu>
2025-06-10 14:25:09 -0400

    feat: add additional redirects for sterling neighborhood storymap (ns-2024)

--- src/redirects.ts
+++ src/redirects.ts
@@ -1,3 +1,7 @@
 export const redirects: Record<string, string> = {
   '/rcdst': '/research/rural-change-diagnostic-screen',
+  '/sterling/portal':
+    'https://gis.furman.edu/portal/apps/storymaps/stories/9668f9a4f3734f748eaffb474e6073a8',
+  '/sterling/online': 'https://storymaps.arcgis.com/stories/9668f9a4f3734f748eaffb474e6073a8',
+  '/sterling': '/sterling/online',
 };
