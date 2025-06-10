7acbba8956f05f1413a1f0b81a0ac6f7afa2256f
Jack Buehner <jackb@furman.edu>
2025-06-10 19:07:14 -0400

    feat: add sterling methods storymap redirect

--- src/redirects.ts
+++ src/redirects.ts
@@ -1,5 +1,7 @@
 export const redirects: Record<string, string> = {
   '/rcdst': '/research/rural-change-diagnostic-screen',
+
+  // sterling-24 storymaps
   '/sterling/portal':
     'https://gis.furman.edu/portal/apps/storymaps/stories/38fe0bebd8124cdab81b2e5b84510303',
   '/sterling/online': 'https://storymaps.arcgis.com/stories/38fe0bebd8124cdab81b2e5b84510303',
@@ -7,6 +9,7 @@ export const redirects: Record<string, string> = {
   '/sterling/transportation/portal': 'https://gis.furman.edu/portal/apps/storymaps/stories/399e1df1f0104fb69ce9f7260e4b1a0c',
   '/sterling/transportation/online': 'https://storymaps.arcgis.com/stories/399e1df1f0104fb69ce9f7260e4b1a0c',
   '/sterling/transportation': '/sterling/transportation/online',
+  '/sterling/methods': 'https://storymaps.arcgis.com/stories/e3392a2035684ac697d178cc8755751a',
 
   // northside-24 storymap
   '/northside': 'https://storymaps.arcgis.com/stories/1322a6f2788142eda234c28cabe05505',
