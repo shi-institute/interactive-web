165912e77b9194766a2c2f8819f7802e69c8e1c8
Jack Buehner <github@jackbuehner.com>
2025-06-18 16:49:30 -0400

    Revert "feat: northside/sterling storympa redirects to the portal (#45)"
    This reverts commit bc2bc6e7ec51289ee51b57374d527c46a15d1fa3.

--- src/redirects.ts
+++ src/redirects.ts
@@ -5,14 +5,14 @@ export const redirects: Record<string, string> = {
   '/sterling/portal':
     'https://gis.furman.edu/portal/apps/storymaps/stories/38fe0bebd8124cdab81b2e5b84510303',
   '/sterling/online': 'https://storymaps.arcgis.com/stories/38fe0bebd8124cdab81b2e5b84510303',
-  '/sterling': '/sterling/portal',
+  '/sterling': '/sterling/online',
   '/sterling/transportation/portal': 'https://gis.furman.edu/portal/apps/storymaps/stories/399e1df1f0104fb69ce9f7260e4b1a0c',
   '/sterling/transportation/online': 'https://storymaps.arcgis.com/stories/399e1df1f0104fb69ce9f7260e4b1a0c',
-  '/sterling/transportation': '/sterling/transportation/portal',
-  '/sterling/methods': 'https://gis.furman.edu/portal/apps/storymaps/stories/e3392a2035684ac697d178cc8755751a',
+  '/sterling/transportation': '/sterling/transportation/online',
+  '/sterling/methods': 'https://storymaps.arcgis.com/stories/e3392a2035684ac697d178cc8755751a',
 
   // northside-24 storymap
-  '/northside': 'https://gis.furman.edu/portal/apps/storymaps/stories/1322a6f2788142eda234c28cabe05505',
-  '/northside/transportation': 'https://gis.furman.edu/portal/apps/storymaps/stories/17bf269c02af45679fbcaaecbef77c87',
-  '/northside/methods': 'https://gis.furman.edu/portal/apps/storymaps/stories/e20e06f7043743a9bc8f4d74bf9adecc',
+  '/northside': 'https://storymaps.arcgis.com/stories/1322a6f2788142eda234c28cabe05505',
+  '/northside/transportation': 'https://storymaps.arcgis.com/stories/17bf269c02af45679fbcaaecbef77c87',
+  '/northside/methods': 'https://storymaps.arcgis.com/stories/e20e06f7043743a9bc8f4d74bf9adecc',
 };
