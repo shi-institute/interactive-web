b8567ff56e1c8e134a79f457e951f184d73a47dc
Jack Buehner <jackb@furman.edu>
2025-09-09 11:32:44 -0400

    feat: add northside-24 and sterling-24 storymap embeds

--- src/redirects.ts
+++ src/redirects.ts
@@ -2,17 +2,20 @@ export const redirects: Record<string, string> = {
   '/rcdst': '/research/rural-change-diagnostic-screen',
 
   // sterling-24 storymaps
-  '/sterling/portal':
-    'https://gis.furman.edu/portal/apps/storymaps/stories/38fe0bebd8124cdab81b2e5b84510303',
-  '/sterling/online': 'https://storymaps.arcgis.com/stories/38fe0bebd8124cdab81b2e5b84510303',
-  '/sterling': '/sterling/online',
-  '/sterling/transportation/portal': 'https://gis.furman.edu/portal/apps/storymaps/stories/399e1df1f0104fb69ce9f7260e4b1a0c',
-  '/sterling/transportation/online': 'https://storymaps.arcgis.com/stories/399e1df1f0104fb69ce9f7260e4b1a0c',
-  '/sterling/transportation': '/sterling/transportation/online',
-  '/sterling/methods': 'https://storymaps.arcgis.com/stories/e3392a2035684ac697d178cc8755751a',
+  '/sterling/portal': '/sterling',
+  '/sterling/online': '/sterling',
+  '/sterling': '/research/sterling-24',
+  '/sterling/transportation/portal': '/sterling/transportation',
+  '/sterling/transportation/online': '/sterling/transportation',
+  '/sterling/transportation':
+    'https://gis.furman.edu/portal/apps/storymaps/stories/399e1df1f0104fb69ce9f7260e4b1a0c',
+  '/sterling/methods':
+    'https://gis.furman.edu/portal/apps/storymaps/stories/e3392a2035684ac697d178cc8755751a',
 
   // northside-24 storymap
-  '/northside': 'https://storymaps.arcgis.com/stories/1322a6f2788142eda234c28cabe05505',
-  '/northside/transportation': 'https://storymaps.arcgis.com/stories/17bf269c02af45679fbcaaecbef77c87',
-  '/northside/methods': 'https://storymaps.arcgis.com/stories/e20e06f7043743a9bc8f4d74bf9adecc',
+  '/northside': '/research/northside-24',
+  '/northside/transportation':
+    'https://gis.furman.edu/portal/apps/storymaps/stories/17bf269c02af45679fbcaaecbef77c87',
+  '/northside/methods':
+    'https://gis.furman.edu/portal/apps/storymaps/stories/e20e06f7043743a9bc8f4d74bf9adecc',
 };
---
+++ src/routes/research/sterling-24/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Frame from '$lib/Frame.svelte';
+</script>
+
+<svelte:head>
+  <title>Sterling Neighborhood Study – Greenville, SC</title>
+</svelte:head>
+
+<Frame
+  src="https://gis.furman.edu/portal/apps/storymaps/stories/38fe0bebd8124cdab81b2e5b84510303"
+/>
