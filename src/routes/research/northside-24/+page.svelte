b1c0eee40c5128f5d0b611b818d6865c282d1cbb
Jack Buehner <jackb@furman.edu>
2025-09-09 12:47:02 -0400

    feat: add northside-24 and sterling-24 storymap embeds (#49)
    * feat: add northside-24 and sterling-24 storymap embeds
    
    * chore: set node version to 20

---
+++ src/routes/research/northside-24/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Frame from '$lib/Frame.svelte';
+</script>
+
+<svelte:head>
+  <title>Northside Neighborhood Study – Spartanburg, SC</title>
+</svelte:head>
+
+<Frame
+  src="https://gis.furman.edu/portal/apps/storymaps/stories/1322a6f2788142eda234c28cabe05505"
+/>
